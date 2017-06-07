ff44749ee9adf72ee5a3e7f04490d0eb28772cf1
Ziga Patacko Koderman <ziga.patacko@protonmail.com>
2017-06-07 23:36:24 +0200

    try relative paths

--- build/windows/build.sh
+++ build/windows/build.sh
@@ -2,6 +2,9 @@
 
 set -xue
 
+cd
+cd libelectronpass
+
 MXE_DIR=/usr/lib/mxe
 
 if [ "$BATTLESHIP_PLATFORM" = "windows32" ]; then
@@ -12,7 +15,7 @@ if [ "$BATTLESHIP_PLATFORM" = "windows64" ]; then
     MXE_TARGET=x86_64-w64-mingw32.static
 fi
 
-${MXE_DIR}/usr/bin/${MXE_TARGET}-cmake . -B"build-dir/libelectronpass"
-cmake --build "build-dir/libelectronpass"
+${MXE_DIR}/usr/bin/${MXE_TARGET}-cmake .
+cmake --build
 
 set +xue
