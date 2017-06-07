f48f3f5fb55a22885113e17357258eb374a0bfce
Ziga Patacko Koderman <ziga.patacko@protonmail.com>
2017-06-07 21:19:41 +0200

    fix package names

--- build/windows/requirements.sh
+++ build/windows/requirements.sh
@@ -24,7 +24,7 @@ fi
 MXE2_TARGET=$(echo "$MXE_TARGET" | sed 's/_/-/g')
 sudo apt-get --yes install \
     mxe-${MXE2_TARGET}-qt \
-    libsodium
+    mxe-${MXE2_TARGET}-libsodium
 
 # MXE doesn't have 64bit NSIS
 sudo apt-get --yes install \
