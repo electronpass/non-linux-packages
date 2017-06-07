0180f3ffa2c97d541f89e492e3ce5ddb07583d32
Ziga Patacko Koderman <ziga.patacko@protonmail.com>
2017-06-07 21:12:20 +0200

    try building libelectronpass

---
+++ build/windows/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -xue
+
+MXE_DIR=/usr/lib/mxe
+
+if [ "$BATTLESHIP_PLATFORM" = "windows32" ]; then
+    MXE_TARGET=i686-w64-mingw32.static
+fi
+
+if [ "$BATTLESHIP_PLATFORM" = "windows64" ]; then
+    MXE_TARGET=x86_64-w64-mingw32.static
+fi
+
+${MXE_DIR}/usr/bin/${MXE_TARGET}-cmake . -B"build-dir/libelectronpass"
+cmake --build "build-dir/libelectronpass"
+
+set +xue
