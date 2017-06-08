b2db8ec6751690b01d9c1ba4d627702c998b8f1c
Ziga Patacko Koderman <ziga.patacko@protonmail.com>
2017-06-08 12:03:31 +0200

    add ecm and electronpass-dekstop

--- build/windows/build.sh
+++ build/windows/build.sh
@@ -2,22 +2,39 @@
 
 set -xue
 
-cd $TRAVIS_BUILD_DIR
-cp CMakeLists-win-libelectronpass.txt libelectronpass/CMakeLists.txt # apply patch
-mkdir libelectronpassinstall
-cd libelectronpass
-
 MXE_DIR=/usr/lib/mxe
 
 if [ "$ELECTRONPASS_PLATFORM" = "windows32" ]; then
-    MXE_TARGET=i686-w64-mingw32.static
+  MXE_TARGET=i686-w64-mingw32.static
 fi
 
 if [ "$ELECTRONPASS_PLATFORM" = "windows64" ]; then
-    MXE_TARGET=x86_64-w64-mingw32.static
+  MXE_TARGET=x86_64-w64-mingw32.static
 fi
 
-${MXE_DIR}/usr/bin/${MXE_TARGET}-cmake -DCMAKE_INSTALL_PREFIX=${TRAVIS_BUILD_DIR}/libelectronpassinstall .
+# install libelectronpass
+cd $TRAVIS_BUILD_DIR
+git clone https://github.com/electronpass/libelectronpass.git
+cp CMakeLists-win-libelectronpass.txt libelectronpass/CMakeLists.txt # apply patch
+cd libelectronpass
+${MXE_DIR}/usr/bin/${MXE_TARGET}-cmake .
+make -j2
+make install
+
+
+# install extra-cmake-modules
+cd $TRAVIS_BUILD_DIR
+git clone https://github.com/KDE/extra-cmake-modules.git
+cd extra-cmake-modules
+${MXE_DIR}/usr/bin/${MXE_TARGET}-cmake .
+make -j2
+make install
+
+# build electronpass-desktop
+cd $TRAVIS_BUILD_DIR
+git clone https://github.com/electronpass/electronpass-desktop.git
+cd electronpass-desktop
+${MXE_DIR}/usr/bin/${MXE_TARGET}-cmake .
 make -j2
 make install
 
